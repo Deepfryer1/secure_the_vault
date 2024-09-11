bc2577f4f1ceff7d010f1a7055f63daa3087d217
Secure Information <securepd@Secures-MacBook-Air.local>
2024-09-11 18:21:59 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("This is my first JavaScript program");
+
+//
+//A combination of three numbers will result from three unique combinations of calculations using arithmetic functions of (+-*/).
+// Comments will be added throughout the  the code to explain each step in the process.
+// A dialog box displaying the vault codes and the text will be provided.
+// Alert only works in the browser not in node
+// alert ("This is an alert")
+
+const message1= "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+console.log (`This exercise will display a string of characters for the user.`)
+const message2="These are some of the building cost calculations"
+alert("This is the alert!")
+constNine=9
+constEight=8
+constSeven=7
+constPiping= seven+1
+constFloors= eight-3
+constCeiling= nine*3
+
+console.log `The first combination code is ${constNine}-constPiping`
+console.log `The second combination code is ${constEight}+constFloors`
+console.log `The third  combination  code is ${constSeven}*constCeiling`
